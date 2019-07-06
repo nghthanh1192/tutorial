12f44d55905329d7141c0d75b9fb4a8db7d1673e
Thanh Nguyen <hthanh1192@gmail.com>
2019-07-06 20:45:01 +0700

    Terminate Midddleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,7 +60,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'Role' => \App\Http\Middleware\RoleMiddleware::class
+        'Role' => \App\Http\Middleware\RoleMiddleware::class,
+        'terminate' => \App\Http\Middleware\TerminateMiddleware::class
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,8 @@ Route::get('role',[
     'middleware' => 'Role:editor',
     'uses' => 'TestController@index',
 ]);
+
+Route::get('terminate', [
+    'middleware' => 'terminate',
+    'uses' => 'ABCController@index',
+]);
